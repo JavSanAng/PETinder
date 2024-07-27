769e721ecf528aa6f71246d5d1d049357aa47d4a
Anpirah <anpira@gmail.com>
2024-07-27 14:05:05 +0200

    front con react iniciado

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Routes from './routes';
+import './App.css'; // Importar el archivo de estilos globales
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/vite-project/src/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/HomePage';
+import Login from './pages/LoginPage';
+
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
