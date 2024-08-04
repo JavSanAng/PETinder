6dfa54bbc4e4c1794ac04ac372862973b5875341
JavSanAng <116178401+JavSanAng@users.noreply.github.com>
2024-08-04 15:00:45 +0200

    Deploy ready

---
+++ frontend/vite-project/src/pages/register/fail.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { makeRequest } from "../../context/axios";
+import "./register.css";
+
+const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await makeRequest.post("/auth/register", {
+        user_name: userName,
+        password: password,
+      });
+
+      console.log("Registration successful:", response.data);
+      navigate("/login");
+    } catch (err) {
+      setError("Registration failed. Please try again.");
+      console.error("Registration error:", err);
+    }
+  };
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>PETinder.</h1>
+          <p>
+          ¡Únete a Petinder hoy y descubre un mundo de amigos peludos! Conéctate, comparte y encuentra el compañero perfecto para ti.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="userName">Username</label>
+            <input
+              type="text"
+              id="userName"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {error && <p className="error">{error}</p>}
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
