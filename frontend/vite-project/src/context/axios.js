a28f8dcfe1e124a4861d112973f27fe3440871c1
JavSanAng <116178401+JavSanAng@users.noreply.github.com>
2024-08-04 14:09:34 +0200

    Deployment process

--- frontend/vite-project/src/context/axios.js
+++ frontend/vite-project/src/context/axios.js
@@ -1,7 +1,26 @@
+// import axios from 'axios';
+
+// const makeRequest = axios.create({
+//   baseURL: 'http://localhost:5090'
+// });
+
+// makeRequest.interceptors.request.use(config => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// }, error => {
+//   return Promise.reject(error);
+// });
+
+// export { makeRequest };
+
 import axios from 'axios';
+const baseURL = import.meta.env.REACT_APP_BACKEND_URL || 'http://localhost:5090';
 
 const makeRequest = axios.create({
-  baseURL: 'http://localhost:5090'
+  baseURL: baseURL,
 });
 
 makeRequest.interceptors.request.use(config => {
